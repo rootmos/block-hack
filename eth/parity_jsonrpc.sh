f84e311fca4bfa411b8415339521c6d80406578f
Gustav Behm <gustav.behm@gmail.com>
2018-04-10 23:48:46 +0200

    Add a KEC method and replace httpie with curl

--- eth/parity_jsonrpc.sh
+++ eth/parity_jsonrpc.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-NODE=:8545
+NODE=http://localhost:8545
 IPC_SOCKET=${IPC_SOCKET-$PWD/.parity/jsonrpc.ipc}
 
 FAUCET=0x4948653997C1cD1AC7c5D604F09271dEC73C13b1
@@ -11,11 +11,9 @@ envelope() {
     jq '{jsonrpc: "2.0", id: "'${2-0}'", method: "'$1'", params: .}'
 }
 
-DEFAULT_MODE=IPC
-
 send() {
-    case ${MODE-$DEFAULT_MODE} in
-        HTTP) http POST $NODE;;
+    case ${MODE-HTTP} in
+        HTTP) curl -H 'Content-Type: application/json' -X POST --silent -d @- $NODE;;
         IPC) socat - UNIX-CONNECT:$IPC_SOCKET;;
     esac
 }
@@ -36,5 +34,10 @@ getBalance() {
     jq -n '["'$1'"]' \
         | envelope eth_getBalance \
         | send | handle_response \
-        | hex_to_dec
+        | hex_to_dec \
+        | wei_to_ether
+}
+
+kec() {
+    rhash --printf="%@{sha3-256}" -
 }
