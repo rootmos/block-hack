20fca56075ac7bf3807bd5524cf8efaca2f4941a
Gustav Behm <gustav.behm@gmail.com>
2018-04-12 11:49:03 +0200

    Add some keyfile mangling functions

--- eth/parity_jsonrpc.sh
+++ eth/parity_jsonrpc.sh
@@ -6,7 +6,7 @@ NODE=http://localhost:8545
 IPC_SOCKET=${IPC_SOCKET-$PWD/.parity/jsonrpc.ipc}
 
 FAUCET=0x4948653997C1cD1AC7c5D604F09271dEC73C13b1
-export FAUCET_PUB=04f4b083594ce3736b48076058d77301b84248453e9c99d7df7038f7ef3e54d43c32010a0a818073a6bcc7c7f42e77647232cb1f89733e735bda3f7ce9a64a50d7
+export FAUCET_PUB=f4b083594ce3736b48076058d77301b84248453e9c99d7df7038f7ef3e54d43c32010a0a818073a6bcc7c7f42e77647232cb1f89733e735bda3f7ce9a64a50d7
 
 envelope() {
     jq '{jsonrpc: "2.0", id: "'${2-0}'", method: "'$1'", params: .}'
@@ -52,7 +52,7 @@ hex_to_bin() {
 }
 
 address_from_pub() {
-    echo -n $1 | cut -c3- | hex_to_bin | kec | bin_to_hex | tail -c40
+    hex_to_bin | kec | bin_to_hex | tail -c40
 }
 
 tx_hash() {
@@ -64,4 +64,35 @@ mk_tx() {
     j+='"gasLimit":"'$2'","to":"'$3'",'
     j+='"value":"'$4'","data":"'$5'"}'
     echo -n $j | jq .
+}
+
+new_pem_key() {
+    openssl ecparam -name secp256k1 -genkey -noout -out $1.pem
+}
+
+pub_from_pem() {
+    openssl ec -in $1 --pubout -outform DER
+}
+
+address_from_pub() {
+    bin_to_hex | address_from_pub
+}
+
+priv_from_keyfile() {
+    password_raw=faucet
+    password=$(echo -n $password_raw | base91)
+    salt=$(jq -r '.crypto.kdfparams.salt' < $1 | hex_to_bin | base91)
+    salt_size=$(jq -r '.crypto.kdfparams.salt' < $1 | hex_to_bin | wc -c)
+    N=$(jq -r '.crypto.kdfparams.n' < $1)
+    p=$(jq -r '.crypto.kdfparams.p' < $1)
+    r=$(jq -r '.crypto.kdfparams.r' < $1)
+    size=$(jq -r '.crypto.kdfparams.dklen' < $1)
+    derived_key=$(scrypt-kdf --base91-input $password $salt $N $p $r $size $salt_size | base91 --decode | bin_to_hex)
+    key=$(echo -n $derived_key | head -c8)
+    cipher=$(jq -r '.crypto.cipher' < $1)
+    iv=$(jq -r '.crypto.cipherparams.iv' < $1)
+    jq -r '.crypto.ciphertext' < $1 \
+        | hex_to_bin \
+        | openssl enc -$cipher -iv $iv -K $key -d \
+        | bin_to_hex
 }
